7c261c475f2d52585c9e6788764259cd16ea2dfd
xavier dupré <xavier.dupre@gmail.com>
2018-07-10 11:46:58 +0200

    remove submodule machinelearning, taking a dependency on Microsoft.ML nuget package

--- build.cmd
+++ build.cmd
@@ -17,40 +17,21 @@ cd ..
 
 :bboost:
 @echo [build.cmd] build xgboost
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 cd xgboost
 cd build
 msbuild xgboost.sln
 cd ..
 cd ..
 
-:maclea:
-@echo [build.cmd] build machinelearning
-cd machinelearning
-if exist bin\x64.Release goto mldeb:
-cmd /C build.cmd -release
-:mldeb:
-if exist bin\x64.Debug goto mlrel:
-cmd /C build.cmd -debug
-:mlrel:
-cd ..
-
-if not exist machinelearning\bin\x64.Debug goto end:
 
 @echo [build.cmd] Publish Release
-if not exist machinelearning\dist\Release mkdir machinelearning\dist\Release
-copy machinelearning\bin\AnyCPU.Release\Microsoft.ML.Api\netstandard2.0\*.dll machinelearning\dist\Release
-copy machinelearning\bin\AnyCPU.Release\Microsoft.ML.Maml\netstandard2.0\*.dll machinelearning\dist\Release
-copy machinelearning\bin\AnyCPU.Release\Microsoft.ML\netstandard2.0\*.dll machinelearning\dist\Release
-copy machinelearning\bin\x64.Release\Native\*.dll machinelearning\dist\Release
-copy xgboost\lib\*.dll machinelearning\dist\Release
+if not exist dist\Release mkdir dist\Release
+copy xgboost\lib\*.dll dist\Release
 
 @echo [build.cmd] Publish Debug
-if not exist machinelearning\dist\Debug mkdir machinelearning\dist\Debug
-copy machinelearning\bin\AnyCPU.Debug\Microsoft.ML.Api\netstandard2.0\*.dll machinelearning\dist\Debug
-copy machinelearning\bin\AnyCPU.Debug\Microsoft.ML.Maml\netstandard2.0\*.dll machinelearning\dist\Debug
-copy machinelearning\bin\AnyCPU.Debug\Microsoft.ML\netstandard2.0\*.dll machinelearning\dist\Debug
-copy machinelearning\bin\x64.Debug\Native\*.dll machinelearning\dist\Debug
-copy xgboost\lib\*.dll machinelearning\dist\Debug
+if not exist machinelearning\dist\Debug mkdir dist\Debug
+copy xgboost\lib\*.dll dist\Debug
 
 @echo [build.cmd] build machinelearning_xgboost
 cd machinelearning_xgboost
